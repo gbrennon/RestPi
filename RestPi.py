5b100086153f297352b2cf2a8d0eeca65d3bde2b
gbrennon <glauberbrennon@gmail.com>
2014-05-14 06:51:53 -0300

    RestPi is online!

---
+++ RestPi.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+import RPi.GPIO as GPIO
+
+app = Flask(__name__)
+app.config.from_object('config')
+GPIO.setmode(GPIO.BOARD)
+
+@app.route("/")
+def hello():
+	return "Hello world!" 
+@app.route("/setInput")
+def setInput():
+	GPIO.setup(int(request.args.get('pin')), GPIO.IN)
+	return request.args.get('pin') + " is Input!"
+
+@app.route("/setOutput")
+def setOutput():
+        GPIO.setup(int(request.args.get('pin')), GPIO.OUT)
+        return request.args.get('pin') + " is Output!"
+
+@app.route("/readPin")
+def readPin():
+	value = GPIO.input(request.args.get('pin'))
+	return "Pin value is" + value
+@app.route("/writePin")
+def writePin():
+	GPIO.output(request.args.get('pin'), request.args.get('state'))
+	return request.args.get('pin') + " is " + request.args.get('state') 
+
+
+if __name__ == "__main__":
+	app.run()
