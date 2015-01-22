9c9d529da2e8e03963fa833b66165015b49a5a0f
Gláuber Brennon <glauberbrennon@gmail.com>
2015-01-22 15:40:13 -0200

    New readme

--- README.md
+++ README.md
@@ -3,4 +3,34 @@ RestPi
 
 Minimalist REST API built with Flask!
 
-Documentation will be uploaded as soon as possible!
+Start the server with
+
+```
+sudo python RestPi.py
+```
+
+Check if the server is online:
+
+```
+<raspberry_ip>:5000/
+```
+
+Set a pin 5 as Input:
+
+```
+<raspberry_ip>:5000/setInput?pin=5
+```
+
+Read pin 5 value:
+
+```
+<raspberry_ip>:5000/readPin?pin=5
+```
+
+Set pin 6 to Output and set the state(1 = on, 0 = off):
+
+
+```
+<raspberry_ip>:5000/setOutput?pin=6
+<raspberry_ip>:5000/writePin?pin=6&state=1
+```
--- RestPi.py
+++ RestPi.py
@@ -4,29 +4,35 @@ import RPi.GPIO as GPIO
 app = Flask(__name__)
 GPIO.setmode(GPIO.BOARD)
 
+
 @app.route("/")
 def helloRestPi():
-	return "Welcome to RestPi! A REST API for Raspberry Pi!" 
+    return "Welcome to RestPi! A REST API for Raspberry Pi!"
+
+
 @app.route("/setInput")
 def setInput():
-	GPIO.setup(int(request.args.get('pin')), GPIO.IN)
-	return request.args.get('pin') + " is Input!"
+    GPIO.setup(int(request.args.get('pin')), GPIO.IN)
+    return request.args.get('pin') + " is Input!"
+
 
 @app.route("/setOutput")
 def setOutput():
-        GPIO.setup(int(request.args.get('pin')), GPIO.OUT)
-        return request.args.get('pin') + " is Output!"
+    GPIO.setup(int(request.args.get('pin')), GPIO.OUT)
+    return request.args.get('pin') + " is Output!"
 
 @app.route("/readPin")
 def readPin():
-	value = GPIO.input(int(request.args.get('pin')))
-	return "Pin value is" + value
+    value = GPIO.input(int(request.args.get('pin')))
+    return "Pin value is" + value
+
+
 @app.route("/writePin")
 def writePin():
-	pin = int(request.args.get('pin'))
-	state = int(request.args.get('state'))
-	GPIO.output(pin, state)
-	return pin + " is " + state 
+    pin = int(request.args.get('pin'))
+    state = int(request.args.get('state'))
+    GPIO.output(pin, state)
+    return pin + " is " + state
 
 if __name__ == "__main__":
 	app.run()
